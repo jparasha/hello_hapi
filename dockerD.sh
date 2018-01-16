0eba679531c76df45fbd2df52e04a9217a2e0e1c
jparasha <jayant.parashar@capgemini.com>
2018-01-16 14:37:03 +0530

    fixed empty container id issue

--- dockerD.sh
+++ dockerD.sh
@@ -1,3 +1,9 @@
  CONTID=$(docker ps -q)
- docker commit ${CONTID} jenkinsdockerdemo
+ if [ -n "$CONTID" ]; then
+ 	 # Do something when var is non-zero length
+  	docker commit ${CONTID} jenkinsdockerdemo
 
+ else 
+	docker run -d -p 3000:3000 jenkinsdockerdemo
+
+ fi
